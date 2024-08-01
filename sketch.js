ef4674e1e9428893ec8871bb78bd0672ecc5f427
fafamilgrau <00001106107408sp@al.educacao.sp.gov.br>
2024-08-01 07:50:52 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(500, 500);
+      background("white");
+  }
+  
+  function draw() {
+    
+    stroke ("yellow");
+    fill("pink");
+    
+  //console.log ()
+    
+    if (mouseIsPressed) {  rect(mouseX,mouseY,40,30);}
+    
+    
+  
+  }
